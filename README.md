d077c3c566e96a6899e08fd577d07d41c1747e3c
Gabriel Ramos Takeda <gabrieel.rt@gmail.com>
2016-04-05 17:19:58 -0300

    Add Github Pages link to README.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aidax-proficiency-test
+# Live Preview
+http://gabrielrtakeda.github.io/aidax-proficiency-test/
--- widgets/default.js
+++ widgets/default.js
@@ -4,36 +4,38 @@ FinalBoard.Config = {
   dashboards: [{
     title: 'AIDAX',
     widgets: [
-      [
+      [ // First row
         {
-          js: 'widgets/aidax/google-charts/data.js',
-          template: 'widgets/aidax/google-charts/bar-chart.html',
+          js: configBase + 'google-charts/data.js',
+          template: configBase + 'google-charts/bar-chart.html',
           css_class: "bar-chart col-xs-12 col-md-8"
         },
         {
-          template: 'widgets/aidax/components/total-clients.html',
+          template: configBase + 'components/total-clients.html',
           css_class: "two-rows col-xs-12 col-md-4"
         },
-      ], [
+      ],
+      [ // Middle row
         {
-          template: 'widgets/aidax/components/volume-indications.html',
+          template: configBase + 'components/volume-indications.html',
           css_class: "customer-aquisition-goal col-xs-12 col-md-8"
         },
         {
-          template: 'widgets/aidax/components/total-clients-segmentation.html',
+          template: configBase + 'components/total-clients-segmentation.html',
           css_class: "total-clients-segmentation col-xs-12 col-md-4"
         },
-      ], [
+      ],
+      [ // Last row
         {
-          template: 'widgets/aidax/components/conversion-without-indication.html',
+          template: configBase + 'components/conversion-without-indication.html',
           css_class: "conversion-without-indication col-xs-12 col-md-4"
         },
         {
-          template: 'widgets/aidax/components/conversion-with-indication.html',
+          template: configBase + 'components/conversion-with-indication.html',
           css_class: "conversion-with-indication col-xs-12 col-md-4"
         },
         {
-          template: 'widgets/aidax/components/average-indication-per-person.html',
+          template: configBase + 'components/average-indication-per-person.html',
           css_class: "average-indication-per-person col-xs-12 col-md-4"
         },
       ]
